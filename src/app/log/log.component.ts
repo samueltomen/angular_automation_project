5ecf9cb2d6be0bf01edc5965ccde81a99b4a6372
CarTig <carolle.t.n@gmail.com>
2024-04-23 14:12:15 +0200

    log etape 1

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class LogComponent {
 
+  bathroom = {
+    on:'Salle de bain allumée',
+    off:'Salle de bain éteinte'
+  };
+  livingRoom = { 
+    on: 'Salle à manger allumée', 
+    off: 'Salle à manger éteinte' 
+  };
+  garage = { 
+    on: 'Garage allumé', 
+    off: 'Garage éteint' 
+  };
+  heatPump = { 
+    on: 'Pompe à chaleur allumée', 
+    off: 'Pompe à chaleur éteinte' 
+  };
+  laundryRoom = { 
+    on: 'Buanderie allumée', 
+    off: 'Buanderie éteinte' 
+  };
+  swimmingPool = { 
+    on: 'Piscine allumée', 
+    off: 'Piscine éteinte' 
+  };
+
 }
