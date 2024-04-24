b51382e346cb3ac05f5e39c8d59a4d49a227bf78
William Loyer <w.loyer@outlook.com>
2024-04-24 15:17:09 +0200

    log instruction json

---
+++ src/app/components/instructions/instructions.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-instructions',
+  templateUrl: './instructions.component.html',
+  styleUrls: ['./instructions.component.css']
+})
+export class InstructionsComponent {
+  newInstruction: string = '';
+  instructions: string[] = [];
+
+  addInstruction(): void {
+    if (this.newInstruction) {
+      this.instructions.push(this.newInstruction);
+      this.newInstruction = ''; // Réinitialise l'input après l'ajout
+      // Simuler l'enregistrement dans un fichier en local storage ou envoyer à un backend
+    }
+  }
+}
